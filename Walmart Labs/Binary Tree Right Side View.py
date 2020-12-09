365e08cb636e10c6772df5c89a2e5996d3d762a5
PiyushChandra17 <47579287+PiyushChandra17@users.noreply.github.com>
2020-12-09 20:02:59 +0530

    Add files via upload

---
+++ Walmart Labs/Binary Tree Right Side View.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right):]
+        
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        ans,level = [],[root]
+        while root and level:
+            ans.append(level[-1].val)
+            level = [kid for node in level for kid in (node.left,node.right) if kid]
+        return ans
